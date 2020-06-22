c036637c28fcd852d89cd4386612f2781af5ab90
Kirill <pn79122821224@gmail.com>
2020-06-22 15:55:50 +0500

    chore(ci): removes code coverage

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -22,10 +22,8 @@ module.exports = (on, config) => {
 
 // cypress/plugins/index.js
 module.exports = (on, config) => {
-  require('@cypress/code-coverage/task')(on, config)
   // include any other plugin code...
 
   // It's IMPORTANT to return the config object
   // with any changed environment variables
-  return config
 }
